feffd06cf2ebe0d398822a01e25db292041c3969
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-06-29 12:32:14 +0300

    iter9

--- app/main.js
+++ app/main.js
@@ -1,15 +1,24 @@
 let app = angular.module('app', []);
 
-app.directive('foo', function() {
-    return {
-        link: function (scope, element, attrs) {
-            element.on('click', function () {
-                if (element.text() === 'foo') {
-                    element.text('bar');
-                } else {
-                    element.text('foo');
-                }
-            })
+app.controller('mainCtrl', function ($scope) {
+    $scope.money1 = '1.22$';
+    $scope.money2 = '$2.33';
+    $scope.money3 = '4.43';
+});
+
+app.filter('moneyFilter', function() {
+    return function(str) {
+        let lastChar = str.slice(-1),
+            firstChar = str.slice(0, 1),
+            slicedPart;
+
+        if (lastChar === '$') {
+            slicedPart = str.slice(0, str.length - 1);
+            return '$' + slicedPart;
+        } else if (firstChar === '$') {
+            return str;
+        } else {
+            return '$' + str;
         }
-    } 
+    }
 });
