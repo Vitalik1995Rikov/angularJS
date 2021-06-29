1d7ce0beb8b13556b0b0441c6f95336a9fe3ef4c
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-06-29 10:41:11 +0300

    iter4

--- app/main.js
+++ app/main.js
@@ -1,4 +1,19 @@
-angular.module('app', []).controller('myCtrl', function($scope) {
-    $scope.hello = 55;
-    $scope.myBook = 'AngularJS';
+let app = angular.module('app', []);
+
+app.controller('firstCtrl', function($scope, myFactory) {
+    console.log('firstCtrl');
+    $scope.myFactory = myFactory;
+    $scope.hello = 'hello world';
+});
+
+app.controller('secondCtrl', function($scope, myFactory) {
+    console.log('secondCtrl');
+    $scope.myFactory = myFactory;
+    $scope.hello = 'hello world';
+});
+
+app.factory('myFactory', function() {
+    return {
+        hello: 'hello world'
+    }
 });
