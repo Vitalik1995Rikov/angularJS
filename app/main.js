319d71d6d14ce203e9142a6207e5ac9d88d73cbf
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-06-30 14:58:01 +0300

    iter19

--- app/main.js
+++ app/main.js
@@ -1,25 +1,15 @@
 let app = angular.module('app', []);
 
-app.controller('firstCtrl', function($scope) {
-    $scope.name = 'Harry';
-    $scope.color = '#333333';
-    $scope.reverse = function () {
-        $scope.name = $scope.name.split('').reverse().join('');
-    };
-});
-
-app.directive('fooBar', function(){
+app.directive('wrapIn', function($templateCache) {
     return {
-        scope: {
-            name: '@',
-            color: '=',
-            reverse: '&'
-        },
-        template: '<div>My name is {{name}} <input type="text" ng-model="name"></div>' + 
-        '<div>My color is {{color}} <input type="text" ng-model="color"></div>' +
-        '<button ng-click="reverse()">Reverse</button>',
-        link: function (scope, element, attrs) {
-            console.log('fooBar');
+        transclude: 'element',
+        link: function (scope, element, attrs, ctrl, transclude) {
+            let template = $templateCache.get(attrs.wrapIn);
+            let templateElement = angular.element(template);
+            
+            transclude(scope, function (clone) {
+                element.after(templateElement.append(clone));
+            })
         }
     }
 });
