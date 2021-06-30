9a9ec01e9587bd3bb0d09ab71fd1d448232e3e20
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-06-30 10:30:58 +0300

    iter13

--- app/main.js
+++ app/main.js
@@ -1,12 +1,20 @@
 let app = angular.module('app', []);
 
+app.controller('mainCtrl', function ($scope) {
+    $scope.name = 'Bob'
+});
+
 app.directive('fooBar', function() {
     return {
         restrict: 'E',
         transclude: true,
-        template: 'This is my super directive <div ng-transclude></div>',
-        link: function(scope, element, attrs) {
+        template: 'This is my super directive',
+        link: function(scope, element, attrs, ctrl, transclude) {
             console.log('This is my super directive');
+            transclude(scope, function(clone, scope) {
+                console.log('!', clone, scope);
+                element.append(clone);
+            });
         }
     }
 });
