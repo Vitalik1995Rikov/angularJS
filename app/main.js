15ef0a16f40dc9e1ab6d7070d1efdc29d87a0678
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-01 10:45:26 +0300

    iter23

--- app/main.js
+++ app/main.js
@@ -1,17 +1,1 @@
 let app = angular.module('app', []);
-
-app.directive('uiSource', function () {
-    return {
-        compile: function (elem) {
-            let escape = function (content) {
-                return content.replace(/\</g, '&lt;')
-                              .replace(/\>/g, '&gt;')
-            };
-            let pre = angular.element('<pre class="prettyprint"></pre>');
-            let pretty = prettyPrintOne(escape(elem.html()));
-            console.log(pretty);
-            pre.append(pretty);
-            elem.replaceWith(pre);
-        }
-    };
-});
