5aba0d3fa1bcb3dfd609cd859db9ef0f95bd7c9a
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-01 10:31:41 +0300

    iter22

--- app/main.js
+++ app/main.js
@@ -1,46 +1,17 @@
-let app = angular.module('app', ['ngMockE2E']);
+let app = angular.module('app', []);
 
-app.run(function ($httpBackend) {
-    let books = [
-        {
-            name: 'AngularJS'
-        },
-        {
-            name: 'EmberJS'
-        },
-        {
-            name: 'ReactJS'
+app.directive('uiSource', function () {
+    return {
+        compile: function (elem) {
+            let escape = function (content) {
+                return content.replace(/\</g, '&lt;')
+                              .replace(/\>/g, '&gt;')
+            };
+            let pre = angular.element('<pre class="prettyprint"></pre>');
+            let pretty = prettyPrintOne(escape(elem.html()));
+            console.log(pretty);
+            pre.append(pretty);
+            elem.replaceWith(pre);
         }
-    ];
-
-    $httpBackend.whenGET('http://localhost:3001/books').respond(200, books);
-    $httpBackend.whenPOST('http://localhost:3001/books').respond(function (method, url, data) {
-        let result = JSON.parse(data);
-        books.push(result);
-        return [200, result];
-    });
+    };
 });
-
-app.controller('mainCtrl', function($http, $scope) {
-    $http({
-        method: 'GET',
-        url: 'http://localhost:3001/books'
-    }).then(function (result) {
-        console.log('success', result);
-        $scope.books = result.data;
-    }, function (result) {
-        console.log('error');
-    });
-
-    $scope.addBook = function(book) {
-        $http({
-            method: 'POST',
-            url: 'http://localhost:3001/books'
-        }).then(function (result) {
-            console.log('Book successfully to backend', result);
-            $scope.books.push(book);
-            $scope.book = null;
-        }, function (result) {
-            console.log('Error in book post');
-        });
-}});
