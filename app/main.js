3ccbd02ed10adf5300f7142df0a301ec0860407e
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-01 14:06:49 +0300

    iter25

--- app/main.js
+++ app/main.js
@@ -1,1 +1,19 @@
-let app = angular.module('app', []);
+let app = angular.module('app', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            template: '<h1>This is my homepage</h1> {{model.message}}',
+            controller: 'homeCtrl'
+        })
+        .when('/posts', {
+            template: '<h1>Posts for my site</h1>'
+        })
+});
+
+app.controller('homeCtrl', function ($scope) {
+    console.log('HomeCtrl');
+    $scope.model = {
+        message: 'This is my beautifull homepage'
+    }
+});
