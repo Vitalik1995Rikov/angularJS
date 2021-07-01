d4ad45f9ece862654dbfc220260ece5f4e69aad0
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-07-01 15:01:30 +0300

    iter26

--- app/main.js
+++ app/main.js
@@ -7,7 +7,15 @@ app.config(function ($routeProvider) {
             controller: 'homeCtrl'
         })
         .when('/posts', {
-            template: '<h1>Posts for my site</h1>'
+            template: '<h1>Posts for my site</h1><div ng-repeat="post in posts">{{post.name}}</div>',
+            controller: 'postsCtrl'
+        })
+        .when('/posts/:postId', {
+            template: '<h1>This is my cool post</h1>',
+            controller: 'postCtrl'
+        })
+        .otherwise({
+            template: '<h1>404 no such page<>'
         })
 });
 
@@ -16,4 +24,31 @@ app.controller('homeCtrl', function ($scope) {
     $scope.model = {
         message: 'This is my beautifull homepage'
     }
+});
+
+app.controller('postCtrl', function ($scope, $routeParams) {
+    console.log($routeParams);
+    console.log($routeParams.postId);
+});
+
+app.controller('postsCtrl', function ($scope, postsFactory) {
+    console.log('postsCtrl', postsFactory);
+    $scope.posts = postsFactory;
+});
+
+app.factory('postsFactory', function () {
+    return [
+        {
+           id: 1,
+           name: 'Why AngularJS is good?' 
+        },
+        {
+            id: 2,
+            name: 'Why ReactJS is good?' 
+        },
+        {
+            id: 3,
+            name: 'Why NodeJS is good?' 
+        }
+    ]
 });
